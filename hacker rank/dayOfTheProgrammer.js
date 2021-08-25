0ce66588740cd200f62d37da64925e92b23492de
helman101 <helmanandresortegon@gmail.com>
2021-08-25 18:03:28 +0200

    add solution to day of programmer challenge

---
+++ hacker rank/dayOfTheProgrammer.js
@@ -0,0 +1,9 @@
+function dayOfProgrammer(year) { 
+  if ((year > 1918 && (year%400 === 0 || year%4 === 0 && year%100 != 0)) || year < 1918 && year%4 === 0) {
+      return `12.09.${year}`
+  } else if (year === 1918) {
+      return '26.09.1918'
+  } else {
+      return `13.09.${year}`
+  };
+}
